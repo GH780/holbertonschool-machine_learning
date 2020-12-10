97945102e552c3614cb0703bce65dd58ffdb63be
Gardenia <60855908+GH780@users.noreply.github.com>
2020-12-10 18:56:31 +0200

    Update and rename f4.py to 4-line_up.py

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""function"""
+
+
+def add_arrays(arr1, arr2):
+    """add matrix"""
+    arr = [0 for i in range(len(arr1))] 
+    if (len(arr1) == len(arr2)):
+        arr[i] = arr1[i] + arr2[i]
+        return arr
+    else:
+        return None

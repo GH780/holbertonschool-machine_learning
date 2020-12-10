fc6fb6c8f0b7707d746189940dbf472d29b841dd
Gardenia <60855908+GH780@users.noreply.github.com>
2020-12-10 18:58:41 +0200

    Update 4-line_up.py

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -6,7 +6,8 @@ def add_arrays(arr1, arr2):
     """add matrix"""
     arr = [0 for i in range(len(arr1))] 
     if (len(arr1) == len(arr2)):
-        arr[i] = arr1[i] + arr2[i]
+        for i in range(len arr1):
+            arr[i] = arr1[i] + arr2[i]
         return arr
     else:
         return None

1f3ea7ffd9f1347bf949b20f91ea6abc121f8eed
Gardenia <60855908+GH780@users.noreply.github.com>
2020-12-10 19:10:47 +0200

    Update and rename f5.py to 5-across_the_planes.py

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+module
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    """
+    function two lists
+    """
+
+    result = []
+
+    if len(arr1) != len(arr2):
+        return None
+
+    for i in range(len(arr1)):
+        for j in range(len arr1[i]):
+            result.append(arr1[i] + arr2[i])
+    return result

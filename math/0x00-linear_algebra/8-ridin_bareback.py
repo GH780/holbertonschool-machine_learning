ba04e12ffd00b25db2f8e49ba2cdf1b61e02bb46
Gardenia <60855908+GH780@users.noreply.github.com>
2020-12-10 21:58:09 +0200

    Update 8-ridin_bareback.py

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -1,28 +1,24 @@
 #!/usr/bin/env python3
 """
-module
+Module that multiplies two matrices
 """
 
 
 def mat_mul(mat1, mat2):
     """
-    function
+    Needs a matrix as input
+    Returns a new matrix or None if cannot be multiplied
     """
-
-    if len(mat1) != len(mat2[0]) and len(mat1[0]) != len(mat2):
-        return None
-
-    if len(mat1) == len(mat2[0]):
-        mati = mat2
-        matj = mat1
+    if len(mat1[0]) == len(mat2):
+        new = []
+        for row1 in range(len(mat1)):
+            inner = []
+            for col2 in range(len(mat2[0])):
+                number = 0
+                for col1 in range(len(mat1[0])):
+                    number += (mat1[row1][col1] * mat2[col1][col2])
+                inner.append(number)
+            new.append(inner)
+        return new
     else:
-        mati = mat1
-        matj = mat2
-
-    mat3 = [[0 for i in matj[0]] for j in mati]
-
-    for i in range(len(mati)):
-        for j in range(len(matj[0])):
-            for k in range(len(matj)):
-                mat3[i][j] += mat1[i][k]*mat2[k][j]
-    return mat3
+        return None

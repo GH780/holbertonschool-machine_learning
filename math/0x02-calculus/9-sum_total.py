be8c22eac056cf5fb804aad9d1c2ea3ed5952ff3
Gardenia <60855908+GH780@users.noreply.github.com>
2020-12-13 21:22:37 +0200

    Update 9-sum_total.py

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,10 +4,10 @@
 
 def summation_i_squared(n):
     """sum function"""
-    sum = 0
-    if(n > 1):
-        for i in range(1, n+1):
-            sum += i*i
-        return sum
-    else:
+    if (type(n) is not int) or (n is None) or (n < 1):
         return None
+    else:
+        numbers = range(1, n + 1)
+        result = 0
+        result = map(lambda i: i ** 2, numbers)
+        return sum(result)

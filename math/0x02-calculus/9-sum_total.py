5bead693bb5b266d4fa16a67f7a80e40fc7a01ed
Gardenia <60855908+GH780@users.noreply.github.com>
2020-12-13 21:08:47 +0200

    Update 9-sum_total.py

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,7 +6,7 @@ def summation_i_squared(n):
     """sum function"""
     sum = 0
     if(n > 1):
-        for(i in range(1, n+1)):
+        for i in range(1, n+1):
             sum += i*i
         return sum
     else:

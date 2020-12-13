60046eff1064539c6be7f8ad82146a27e4e9bd7e
Gardenia <60855908+GH780@users.noreply.github.com>
2020-12-13 22:06:54 +0200

    Update 10-matisse.py

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -4,12 +4,18 @@
 
 def poly_derivative(poly):
     """poly"""
-    list = poly.copy()
-    if (len(poly) == 0):
+    if type(poly) is not list or poly == []:
         return None
-    elif (len(poly) == 1):
-        return 0
+    elif len(poly) < 2:
+        return [0]
     else:
-        for i in range(len(poly)):
-            list [i] = i * poly[i]
-        return list[1:]
+        exponent = 1
+        derivative = poly.copy()
+        derivative.pop(0)
+        for i in range(len(derivative)):
+            if type(derivative[i]) is int or type(derivative[i]) is float:
+                derivative[i] = derivative[i] * exponent
+                exponent += 1
+            else:
+                return None
+        return derivative

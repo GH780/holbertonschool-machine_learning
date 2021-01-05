4b70b7ecff7f22bed94616e012b00368fab00f51
Gardenia <60855908+GH780@users.noreply.github.com>
2021-01-05 16:17:12 +0200

    Update 0-neuron.py

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """this module foe neuron class"""
+import numpy as np
 
 
 class Neuron:
     """Neuron class"""
+
     def __init__(self, nx):
         """this function is __init__"""       
         if type(nx) is not int:

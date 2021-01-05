50a1e4cff274461d7ea5092620a205093c0bb92b
Gardenia <60855908+GH780@users.noreply.github.com>
2021-01-05 16:25:11 +0200

    Update 0-neuron.py

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -4,16 +4,20 @@ import numpy as np
 
 
 class Neuron:
-    """Neuron class"""
+    """
+    A class that defines a single neuron
+    """
 
     def __init__(self, nx):
-        """this function is __init__"""       
+        """
+        class constructor
+        :param nx: is the number of input features to the neuron
+        """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
-        elif nx < 1:
+        if nx < 1:
             raise ValueError("nx must be a positive integer")
-        else:
-            self.nx = nx
-        self.w = np.random.randn(1, nx)
+        self.nx = nx
+        self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0

c2158e9f98f5f82301ee3e369d6a38b90bb44121
Gardenia <60855908+GH780@users.noreply.github.com>
2021-01-14 20:04:27 +0200

    Create 5-momentum.py

---
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -0,0 +1,14 @@
+  
+#!/usr/bin/env python3
+"""
+Updates a variable using the gradient descent with momentum
+"""
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    a function that uses momentum optimization algorithm
+    """
+    momentum = beta1 * v + (1 - beta1) * grad
+    updated = var - alpha * momentum
+    return updated, momentum
